82efa94eccc4865558684a651ffc591e852c8b36
Ghan <87141877@qq.com>
2018-03-12 16:35:58 +0800

    修改小BUG

--- app.ts
+++ app.ts
@@ -1,17 +1,15 @@
-import * as Koa from 'koa';
-import * as bodyParser from 'koa-bodyparser';
-import routes from './controller';
+import * as Koa from "koa";
+import * as bodyParser from "koa-bodyparser";
+import routes from "./controller";
 
 const app = new Koa();
 
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    let 
-        start = new Date().getTime(),
-        execTime;
+    const start = new Date().getTime();
     await next();
-    execTime = new Date().getTime() - start;
-    ctx.response.set('X-Response-Time', `${execTime}ms`);
+    const execTime = new Date().getTime() - start;
+    ctx.response.set("X-Response-Time", `${execTime}ms`);
 });
 
 app.use(bodyParser());
@@ -20,4 +18,4 @@ app.use(routes.routes());
 
 app.listen(3000);
 
-console.log('server run at port 3000');
+console.log("server run at port 3000");
--- controller.ts
+++ controller.ts
@@ -1,9 +1,9 @@
-import * as Router from 'koa-router';
-import Index from './src/controllers/index';
+import * as Router from "koa-router";
+import Index from "./src/controllers/index";
 
 const router = new Router();
 
-router.get('/index', Index.index);
-router.get('/hello', Index.hello);
+router.get("/index", Index.index);
+router.get("/hello", Index.hello);
 
 export default router;
